23aba3db8e5ba10a1eb1491656d669b0702b6d07
Julian Contreras <jrcontre@mit.edu>
2016-08-01 00:56:16 -0400

    the bash script too

---
+++ cloudpaper_bash.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# either cmd works but the latter is cleaner
+#python cloudpaper_main.py
+python /home/pi/project_cloudpaper/cloudpaper_main.py >> /home/pi/project_cloudpaper/cloudpaper_log.txt 2>&1
+
