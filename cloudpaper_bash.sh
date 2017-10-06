5e6ef9c4394cef256fba3465ff276178256eb8e8
Julian Contreras <jrcontre@mit.edu>
2017-10-05 22:25:29 -0400

    removed praw upgrade line lol

--- cloudpaper_bash.sh
+++ cloudpaper_bash.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-# either cmd works but the latter is cleaner
-#python cloudpaper_main.py
-pip install praw --upgrade
 python /home/pi/project_cloudpaper/cloudpaper_main.py >> /home/pi/project_cloudpaper/cloudpaper_log.txt 2>&1
