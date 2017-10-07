4582722def158e4dc0754b7892024aa18a58cba9
Julian Contreras <jrcontre@mit.edu>
2017-10-06 21:34:20 -0400

    git testing

--- cloudpaper_bash.sh
+++ cloudpaper_bash.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+cd /home/pi/project_cloudpaper/
+git pull >> /home/pi/project_cloudpaper/cloudpaper_log.txt 2>&1
 python /home/pi/project_cloudpaper/cloudpaper_main.py >> /home/pi/project_cloudpaper/cloudpaper_log.txt 2>&1
