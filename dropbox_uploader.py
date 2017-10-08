df0eea85722ae860e82203cc2d62d1931f2574aa
Julian Contreras <jrcontre@mit.edu>
2017-10-08 17:18:46 -0400

    Added duplicate checks

--- dropbox_uploader.py
+++ dropbox_uploader.py
@@ -49,6 +49,22 @@ class DropboxUploader(object):
         # Create a set of all the entry paths
         self.createEntryPathSet()
 
+    def deleteDuplicateWallpapers(self):
+        hashes = {}
+        entriesToDelete = []
+        for entry in self.allEntries:
+            h = entry.content_hash
+            if h in hashes:
+                print("Duplicate file " + entry.path_lower + " matches " + hashes[h])
+                entriesToDelete.append(dropbox.files.DeleteArg(entry.path_lower))
+            else:
+                hashes[h] = entry.path_lower
+
+        # pass the marked entries for batch deletion
+        if (len(entriesToDelete) > 0):
+            print "batch deleting %r duplicate files" % (len(entriesToDelete))
+            self.client.files_delete_batch(entriesToDelete)
+
     def pruneWallpapers(self):
         if not self.shouldPrune:
             print "PRUNING OFF. RETURNING"
@@ -77,7 +93,7 @@ class DropboxUploader(object):
     # record all the existing wallpaper paths, continuing via cursor if needed
     def getAllWallpaperEntries(self):
         entries = []
-        folderResult = self.client.files_list_folder("/wallpaper changer/wallpapers")
+        folderResult = self.client.files_list_folder("/wallpaper changer/wallpapers", include_deleted=False)
 
         if (folderResult is None):
             print "folder result was null"
@@ -140,10 +156,13 @@ class DropboxUploader(object):
         # delete old files
         self.pruneWallpapers()
 
+        self.deleteDuplicateWallpapers()
+
         # housekeeping
         self.maintainSizesOfLogFiles()
 
 if __name__ == '__main__':
     DBU = DropboxUploader()
     DBU.pruneWallpapers()
+    DBU.deleteDuplicateWallpapers()
     # DBU.run()
